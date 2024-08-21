5d6f2a9b4874766e3429eaef481727d86bf5a474
Karan Shingde <karanshingde@gmail.com>
2024-08-22 00:10:13 +0530

    Chat UI updated

--- chat_app.py
+++ chat_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os 
 import json
 from src.retriever import memory_chain
-from src.agents import ipc_agent
+from src.agents import judicial_agent
 from dotenv import load_dotenv
 from qdrant_client import QdrantClient 
 
@@ -13,51 +13,51 @@ qdrant_url = os.getenv('QDRANT_URI')
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
-# Load configuration
-config_file = "config.json"
-with open(config_file, "r") as file:
-    config = json.load(file)
-
-# Qdrant Client
-client = QdrantClient(url=qdrant_url)
-
-# Collection name
-collection_name = config['COLLECTION_NAME']
-
-# Models
-llm = config['LLAMA']
-embedding_model = config['EMBEDDING_MODEL']
 
 def chat_ui():
-    st.title("Nyaynidhi-IPC Bot")
 
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+    st.set_page_config(page_title='NyayNidhi', page_icon='⚖️')
+    st.title("NyayNidhi.AI")
+    st.warning("NyayNidhi can make mistakes. Verify responses before any action.")
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    def process_query(query):
-        if query.lower() == 'exit':
+    # Add a sidebar with radio buttons
+    section_option = st.sidebar.selectbox("Select Data", ["IPC", "BNS"], placeholder="Select Criminal Code")
+    
+    try:
+        # Print the selected data
+        # st.toast(body=f"Selection Section: {section_option}", icon="✅")
+        if "chat_history" not in st.session_state:
             st.session_state.chat_history = []
-        else:
-            response = ipc_agent(query, st.session_state.chat_history, embedding_model, llm, client, collection_name)
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        def process_query(query):
+            st.session_state.chat_history = []
+            
+            response = judicial_agent(query, st.session_state.chat_history, 
+                                    section_type=section_option)
+            
             st.session_state.chat_history.extend({"human":query, "ai":response["output"]})
             return response["output"]
 
-    if prompt := st.chat_input("Ask a query: "):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+        if prompt := st.chat_input("Ask a query: "):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-        response = process_query(prompt)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        with st.chat_message("assistant"):
-            st.markdown(response)
+            response = process_query(prompt)
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            with st.chat_message("assistant"):
+                st.markdown(response)
+        
+    except Exception as e:
+        st.error("Something went wrong! NyayNidhi can't work at this time.")
+        print(e)
 
 if __name__ == '__main__':
     chat_ui()
